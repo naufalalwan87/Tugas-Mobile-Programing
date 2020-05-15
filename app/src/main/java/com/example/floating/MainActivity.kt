3dddc2fc6d4c94235644c972ddfe67cd819c2521
naufalalwan87 <naufalalwan061@gmail.com>
2020-05-15 22:09:06 +0700

    floatig-bar

---
+++ app/src/main/java/com/example/floating/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.floating
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mFab = findViewById<FloatingActionButton>(R.id.alert)
+        mFab.setOnClickListener {
+            Toast.makeText(this@MainActivity, "FAB is clicked...", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
