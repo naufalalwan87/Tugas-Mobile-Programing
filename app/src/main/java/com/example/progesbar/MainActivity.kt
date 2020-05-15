b192cbe77189d92609b321c960cb9fb25baaf7be
naufalalwan87 <naufalalwan061@gmail.com>
2020-05-15 22:12:28 +0700

    progres-bar

---
+++ app/src/main/java/com/example/progesbar/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.progesbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        progressBar3.visibility - View.GONE
+        progressBar4.max
+    }
+}
